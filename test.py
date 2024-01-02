1c487a25260e36e6a706c251afff53632fe7f073
sky71151 <vbtechnieken@gmail.com>
2024-01-02 21:36:29 +0100

    update blackhat image

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from PIL import Image
 import glob
 import numpy as np
 import easyocr
+import test2
 #from paddleocr import PaddleOCR, draw_ocr
 #ocr = PaddleOCR(use_angle_cls=True, lang="en") # need to run only once to download and load model into memory
 
@@ -13,9 +14,9 @@ import easyocr
 reader = easyocr.Reader(['en'])
 
 # Read input image
-img = cv2.imread("belgische.jpg")
-#img = cv2.imread("verkeer.jpg")  
-#img = cv2.imread("bmw.jpg")
+img = cv2.imread("images/belgische.jpg")
+#img = cv2.imread("images/verkeer.jpg")  
+#img = cv2.imread("images/bmw.jpg")
 
 # convert input image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -37,10 +38,14 @@ else:
       # draw bounding rectangle around the license number plate
       cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
       gray_plates = gray[y:y+h, x:x+w]
+      normal_plate = img[y:y+h, x:x+w]
       edges_plates = cv2.Canny(gray_plates, 100, 200)
       color_plates = img[y:y+h, x:x+w]
-      cv2.imwrite('Detected{}.jpg'.format(index), img)
-      cv2.imwrite('Numberplate{}.jpg'.format(index), gray_plates)
+      cv2.imwrite('images/Detected{}.jpg'.format(index), img)
+      cv2.imwrite('images/Numberplate{}.jpg'.format(index), gray_plates)
+      cv2.imwrite('images/normalplate{}.jpg'.format(index), normal_plate)
+      blackhat_image = test2.blackhat_image('images/normalplate{}.jpg'.format(index))
+
    
    # save number plate detected
    sigma = 0.5  # Assign a value to sigma if needed
@@ -53,10 +58,13 @@ else:
    upper = int(min(255, (1.0 + sigma) * median))
 
    edge_image= cv2.Canny(gray_plates, 0.3, 100)
-   cv2.imwrite('edge_images.jpg', edge_image)
-   cv2.imwrite('plateEdges.jpg', edges_plates)
-   text = pytesseract.image_to_string(Image.open('Numberplate.jpg'))
-   result = reader.readtext('Numberplate.jpg')
+   cv2.imwrite('images/edge_images.jpg', edge_image)
+   cv2.imwrite('images/plateEdges.jpg', edges_plates)
+   #text = pytesseract.image_to_string(Image.open('images/Numberplate.jpg'))
+   #result = reader.readtext('images/Numberplate.jpg')
+
+   text = pytesseract.image_to_string(Image.open('blackhat_image.jpg'))
+   result = reader.readtext('blackhat_image.jpg')
    #result2 = ocr.ocr('Numberplate.jpg')
    print(result)
    #print(result2)
---
+++ test2.py
@@ -0,0 +1,18 @@
+import cv2
+
+def blackhat_image(image_path):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Create a structuring element for the blackhat operation
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+    # Perform the blackhat operation
+    blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
+
+    # Save the blackhat image
+    cv2.imwrite("blackhat_image.jpg", blackhat)
+
+# Example usage
+#image_path = "/path/to/image.jpg"
+#blackhat_image(image_path)
